ff641df2a37033c21691e92b5a2bd97d7cd39f62
Justin Yang <justin@y4ng.com>
2019-01-18 23:21:12 -0600

    Add project data description and images

--- static/projectData.js
+++ static/projectData.js
@@ -1,183 +1,183 @@
 module.exports.projectData = [
   {
     "name": "Julia",
-    "desc": "",
-    "image": "",
+    "desc": "Julia is a high-level, high-performance dynamic language for technical computing.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/6/69/Julia_prog_language.svg",
     "label": "contributed",
     "link": "https://github.com/JuliaLang"
   },
   {
     "name": "Apache Mesos",
-    "desc": "",
-    "image": "",
+    "desc": "Apache Mesos abstracts CPU, memory, storage, and other compute resources away from machines.",
+    "image": "https://mesosphere.com/wp-content/themes/mesosphere/library/images/views/why-mesos/mesos-logo.png",
     "label": "contributed",
     "link": "https://github.com/apache/mesos"
   },
   {
     "name": "Apache Airvata",
-    "desc": "",
-    "image": "",
+    "desc": "Apache Airavata is a software framework that enables you to compose, manage, execute, and monitor large scale applications and workflows on distributed computing resources such as local clusters, supercomputers,computational grids, and computing clouds.",
+    "image": "https://cdn-images-1.medium.com/max/1000/1*8IEaaGlSIPAsTgVL7jNZVw.png",
     "label": "contributed",
     "link": "https://airavata.apache.org/index.html"
   },
   {
     "name": "NumPy",
-    "desc": "",
-    "image": "",
+    "desc": "NumPy is a library for the Python programming language, adding support for large, multi-dimensional arrays and matrices, along with a large collection of high-level mathematical functions to operate on these arrays.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/NumPy_logo.svg/1200px-NumPy_logo.svg.png",
     "label": "contributed",
     "link": "https://github.com/numpy"
   },
   {
     "name": "Conda",
-    "desc": "",
-    "image": "",
+    "desc": "Package, dependency and environment management for any language—Python, R, Ruby, Lua, Scala, Java, JavaScript, C/ C++, FORTRAN.",
+    "image": "https://cdn-images-1.medium.com/max/1600/1*_QBvDsMNDkZBt32BE9zENQ.png",
     "label": "contributed",
     "link": "https://github.com/conda"
   },
   {
     "name": "Qtum",
-    "desc": "",
-    "image": "",
+    "desc": "Learn everything about QTUM coin, before you invest in Qtum. Understand what is Qtum how to buy QTUM coin and the future of Qtum cryptocurrency.",
+    "image": "https://qph.fs.quoracdn.net/main-qimg-3679a8e09e41a8f8a61c168cf89e0054",
     "label": "contributed",
     "link": "https://qtum.org/en"
   },
   {
     "name": "Rust",
-    "desc": "",
-    "image": "",
+    "desc": "Rust is a multi-paradigm systems programming language focused on safety, especially safe concurrency. Rust is syntactically similar to C++, but is designed to provide better memory safety while maintaining high performance.",
+    "image": "https://firebounty.com/image/483-rust-lang-the-rust-programming-language",
     "label": "contributed",
     "link": "https://github.com/rust-lang"
   },
   {
     "name": "Jupyter",
-    "desc": "",
-    "image": "",
+    "desc": "Project Jupyter exists to develop open-source software, open-standards, and services for interactive computing across dozens of programming languages.",
+    "image": "https://i2.wp.com/softwareengineeringdaily.com/wp-content/uploads/2018/07/jupyter-logo-featured-image.png",
     "label": "contributed",
     "link": "https://github.com/jupyter"
   },
   {
     "name": "yargs",
-    "desc": "",
-    "image": "",
+    "desc": "Yargs helps you build interactive command line tools by parsing arguments and generating an elegant user interface.",
+    "image": "https://avatars1.githubusercontent.com/u/16504989",
     "label": "contributed",
     "link": "https://github.com/yargs"
   },
   {
     "name": "gulp.js",
-    "desc": "",
-    "image": "",
+    "desc": "gulp is an open-source JavaScript toolkit used as a streaming build system in front-end web development.",
+    "image": "https://codehangar.io/content/images/2015/10/gulp-logo.jpg",
     "label": "contributed",
     "link": "https://github.com/gulpjs/gulp"
   },
   {
     "name": "npm",
-    "desc": "",
-    "image": "",
+    "desc": "npm is the package manager for JavaScript and the world's largest software registry.",
+    "image": "https://clofusinnovations.com/blog/images/npm.jpg",
     "label": "contributed",
     "link": "https://github.com/npm"
   },
   {
     "name": "PrairieLearn",
-    "desc": "",
-    "image": "",
+    "desc": "PrairieLearn is an online problem-driven learning system for creating homeworks and tests.",
+    "image": "https://avatars3.githubusercontent.com/u/4580042",
     "label": "contributed",
     "link": "https://github.com/PrairieLearn/PrairieLearn"
   },
   {
     "name": "Powershell",
-    "desc": "",
-    "image": "",
+    "desc": "PowerShell Core is a cross-platform (Windows, Linux, and macOS) automation and configuration tool/framework that works well with your existing tools and is optimized for dealing with structured data (e.g. JSON, CSV, XML, etc.), REST APIs, and object models.",
+    "image": "https://raw.githubusercontent.com/PowerShell/PowerShell/master/assets/ps_black_64.svg?sanitize=true",
     "label": "contributed",
     "link": "https://github.com/powershell"
   },
   {
     "name": "arcdata",
-    "desc": "",
-    "image": "",
+    "desc": "Incident management and volunteer scheduling application for Red Cross Disaster Services",
+    "image": "http://generatormedia.com/images/case_studies/american_red_cross/american_red_cross_logo.png",
     "label": "contributed",
     "link": "https://github.com/redcross/arcdata"
   },
   {
     "name": "spack",
-    "desc": "",
-    "image": "",
+    "desc": "A flexible package manager that supports multiple versions, configurations, platforms, and compilers.",
+    "image": "https://insidehpc.com/wp-content/uploads/2015/11/spack-300x201.jpg",
     "label": "contributed",
     "link": "https://github.com/spack/spack"
   },
   {
     "name": "Firebase",
-    "desc": "",
-    "image": "",
+    "desc": "Firebase is Google's mobile platform that helps you quickly develop high-quality apps and grow your business.",
+    "image": "https://firebase.google.com/images/social.png",
     "label": "contributed",
     "link": "https://github.com/firebase"
   },
   {
     "name": "pandas",
-    "desc": "",
-    "image": "",
+    "desc": "pandas is an open source, BSD-licensed library providing high-performance, easy-to-use data structures and data analysis tools for the Python programming language.",
+    "image": "https://pandas.pydata.org/_static/pandas_logo.png",
     "label": "contributed",
     "link": "https://github.com/pandas-dev"
   },
   {
-    "name": "donejs",
-    "desc": "",
-    "image": "",
+    "name": "DoneJS",
+    "desc": "DoneJS is an open source JavaScript framework that makes it easy to build high performance, real time web and mobile applications.",
+    "image": "https://camo.githubusercontent.com/deb9140059f2ed68be468d1eaf951f4d1d40ba86/68747470733a2f2f646f6e656a732e636f6d2f7374617469632f696d672f646f6e656a732d6c6f676f2d626c61636b2e737667",
     "label": "contributed",
     "link": "https://github.com/donejs/"
   },
   {
     "name": "now",
-    "desc": "",
-    "image": "",
+    "desc": "Global Serverless Deployments",
+    "image": "https://cdn-images-1.medium.com/max/1200/1*Qn8Hp4jTNlML3Hgv-NcTjg.png",
     "label": "contributed",
     "link": "https://github.com/zeit/now-cli"
   },
   {
-    "name": "puppeteer",
-    "desc": "",
-    "image": "",
+    "name": "Puppeteer",
+    "desc": "Puppeteer is a Node library which provides a high-level API to control headless Chrome or Chromium over the DevTools Protocol.",
+    "image": "https://developers.google.com/web/tools/images/puppeteer.png",
     "label": "contributed",
     "link": "https://github.com/googlechrome/puppeteer"
   },
   {
-    "name": "thefuck",
-    "desc": "",
-    "image": "",
+    "name": "The Fuck",
+    "desc": "Magnificent app which corrects your previous console command.",
+    "image": "https://avatars0.githubusercontent.com/u/2112697",
     "label": "contributed",
     "link": "https://github.com/nvbn/thefuck"
   },
   {
     "name": "Puppeteer to Istanbul",
-    "desc": "",
-    "image": "",
+    "desc": "Convert coverage from the format outputted by puppeteer to a format consumable by Istanbul.",
+    "image": "https://avatars1.githubusercontent.com/u/13523395",
     "label": "created",
     "link": "https://github.com/istanbuljs/puppeteer-to-istanbul"
   },
   {
     "name": "Rust Cookbook",
-    "desc": "",
-    "image": "",
+    "desc": "The Rust Cookbook is a collection of simple examples that demonstrate good practices to accomplish common programming tasks, using the crates of the Rust ecosystem.",
+    "image": "https://avatars2.githubusercontent.com/u/14631425",
     "label": "contributed",
     "link": "https://github.com/rust-lang-nursery/rust-cookbook"
   },
   {
-    "name": "rustbridge",
-    "desc": "",
-    "image": "",
+    "name": "RustBridge",
+    "desc": "RustBridge is a workshop focused on getting underrepresented people with a background in another programming language to learn Rust and join the community.",
+    "image": "https://avatars0.githubusercontent.com/u/16467739",
     "label": "created",
     "link": "https://github.com/rustbridge/rustbridge.io"
   },
   {
     "name": "Law --Less",
-    "desc": "",
-    "image": "",
+    "desc": "Law --Less uses the --Less is More framework to read in legal documents, scanning for the type of infraction, the law in question, and the punishment being faced, among other things.",
+    "image": "https://raw.githubusercontent.com/warut-vijit/law__--less/master/static/icons/logo.png",
     "label": "created",
     "link": "https://github.com/warut-vijit/law__--less"
   },
   {
-    "name": "React Native Emoji Keyboard",
-    "desc": "",
-    "image": "",
+    "name": "React Native Emoji Input",
+    "desc": "A performant, customizable keyboard input for React Native.",
+    "image": "https://miro.medium.com/max/900/1*SluXAlSPIxxWjwUsiqD2Kw.png",
     "label": "created",
     "link": "https://github.com/sskhandek/react-native-emoji-input"
   }
